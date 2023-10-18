82486780c3b9de17d25bc968497a36b6e2737dc5
Uallessonivo <uallessons@gmail.com>
2023-10-18 17:51:47 -0300

    updating

--- main.go
+++ main.go
@@ -1,5 +1,84 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/antchfx/xmlquery"
+)
+
+func seedMap(rows [][]string) map[string]string {
+	m := make(map[string]string)
+
+	for _, row := range rows {
+		fields := strings.Split(row[0], ";")
+		if len(fields) >= 2 {
+			m[fields[0]] = fields[1]
+		}
+	}
+
+	return m
+}
+
+func enterToClose() {
+	fmt.Println("Aperte 'enter' para sair")
+	fmt.Scanln()
+}
+
+func openFile(fileName string) (file *os.File) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Erro ao abrir arquivo: ", fileName)
+		enterToClose()
+	}
+
+	return file
+}
+
 func main() {
-	println("Hello World")
+	file := openFile("base.csv")
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	rows, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println("Erro ao ler arquivo base")
+		enterToClose()
+	}
+
+	m := seedMap(rows)
+
+	var xmlFileName string
+	fmt.Println("Digite o nome do arquivo xml: ")
+	fmt.Scanln(&xmlFileName)
+
+	if !strings.HasSuffix(xmlFileName, ".xml") {
+		xmlFileName += ".xml"
+	}
+
+	f := openFile(xmlFileName)
+	defer f.Close()
+
+	doc, err := xmlquery.Parse(f)
+	if err != nil {
+		fmt.Println("Erro ao ler arquivo xml")
+		enterToClose()
+	}
+
+	nodes := xmlquery.Find(doc, "//cProd")
+
+	for _, node := range nodes {
+		code := node.InnerText()
+
+		if value, ok := m[code]; ok {
+			node.FirstChild.Data = value
+		}
+	}
+
+	// TODO: Create the xml file
+
+	fmt.Println("Arquivo xml atualizado com sucesso")
+	enterToClose()
 }
